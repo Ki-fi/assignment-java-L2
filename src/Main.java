166a527d72b055e33aa136b0e1657ec859450bf5
Ki-fi <k.meekels@gmail.com>
2025-03-20 12:26:23 +0100

    Puntjes op de i

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int number;
+        int number1;
+        int number2;
         int input1;
         int input2;
         String name;
 
         hello();
+
+        positiveOrNegative(6);
+
+        postiveOrZeroOrNegative(0);
+
+        bartender("Henk");
+
+        sum(4, 20);
+
     }
 
     public static void hello(){
@@ -21,21 +31,31 @@ public class Main {
         }
     }
 
-    public static void postiveOrZeroOrNegativ(int number){
-        if(number < 0){
+    public static void postiveOrZeroOrNegative(int number2) {
+        if (number2 < 0) {
             System.out.println("Negative number");
-        } else if(number != 0){
+        } else if (number2 == 0) {
             System.out.println("Number is zero");
         } else {
             System.out.println("Positive number");
+        }
     }
 
     public static void bartender(String name){
-
+        switch (name){
+            case "Henk":
+                System.out.println("Henk is de BOB");
+                break;
+            case "Vladimir":
+                System.out.println("Vladimir drinkt alleen vodka");
+                break;
+            default:
+                System.out.println("Invalid name");
+        }
     }
 
     public static void sum(int input1, int input2){
-
+        System.out.println(input1 + input2);
     }
 
 
