681287cac06dcf49e7fef3caacc0c0ed0f0d9d61
Ki-fi <k.meekels@gmail.com>
2025-03-20 11:49:13 +0100

    Opzetje eerste classes

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        int number;
+        int input1;
+        int input2;
+        String name;
+
+        hello();
+    }
+
+    public static void hello(){
+        System.out.println("Hello World");
+    }
+
+
+    public static void positiveOrNegative(int number){
+        if(number < 0){
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Positive number");
+        }
+    }
+
+    public static void postiveOrZeroOrNegativ(int number){
+        if(number < 0){
+            System.out.println("Negative number");
+        } else if(number != 0){
+            System.out.println("Number is zero");
+        } else {
+            System.out.println("Positive number");
+    }
+
+    public static void bartender(String name){
+
+    }
+
+    public static void sum(int input1, int input2){
+
+    }
+
+
+
+
+}
